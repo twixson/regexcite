8994ad40cabe11bddfa2564a6f4b6545809d0115
twixson <troy.wixson12@gmail.com>
2023-06-02 15:16:08 -0600

    Change name of strsplit1() and add error handling

---
+++ R/str_split_one.R
@@ -0,0 +1,26 @@
+#' Split a string
+#' @description
+#' This function is identical to `strsplit()` but returns a `vector` rather than a `list`.
+#'
+#'
+#' @param string A character vector with, at most, one element.
+#' @inheritParams stringr::str_split
+#'
+#' @return A character Vector.
+#' @export
+#'
+#' @examples
+#' x <- "alfa,bravo,charlie,delta"
+#' str_split_one(x, split = ",")
+#' str_split_one(x, split = ",", n=2)
+#'
+#' y <- c("192.168.0.1")
+#' str_split_one(y, pattern = stringr::fixed("."))
+str_split_one <- function(string, pattern, n = Inf) {
+  stopifnot(is.character(string), length(string) <= 1)
+  if (length(string) == 1) {
+    stringr::str_split(string = string, pattern = pattern, n = n)[[1]]
+  } else {
+    character()
+  }
+}
